1811b9db2b0caf803b66bf3667ffae1d9869c1e3
mansi-11-02 <mansi.10747874@ltimindtree.com>
2025-05-30 14:24:54 +0530

    Update todo_list.py

--- src/todo_list.py
+++ src/todo_list.py
@@ -1,6 +1,9 @@
+import os  # Unused import
+
 class TodoList:
     def __init__(self):
         self.todos = []
+        self.password = "12345"  # Hardcoded password
 
     def add_todo(self, todo):
         self.todos.append(todo)
@@ -11,3 +14,13 @@ class TodoList:
 
     def get_todos(self):
         return self.todos
+
+    def complex_condition(self, todo):
+        if todo in self.todos and len(todo) > 5 and todo.startswith("A"):
+            return True
+        return False
+
+    def duplicate_code(self):
+        # Duplicate code block
+        self.todos.append("Sample Todo")
+        self.todos.append("Sample Todo")
